568629cdd7b0ba9a8f3e02c83b55b3a9eae32ca6
Yusuke Kamiyamane <y@y14e.com>
2026-04-27 11:08:35 +0900

    v3.2.1

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * High-performance deep merge utility with structural sharing.
  * Supports circular ref and complex built-in types.
  *
- * @version 3.2.0
+ * @version 3.2.1
  * @author Yusuke Kamiyamane
  * @license MIT
  * @copyright Copyright (c) 2026 Yusuke Kamiyamane
